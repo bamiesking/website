3970030382be87247583e51a77fb19511c3fa53e
bamiesking <bamiesking@gmail.com>
2020-09-29 21:38:02 +0100

    Changed /cv route to point to new DigitalOcean space

--- main/routes.py
+++ main/routes.py
@@ -19,7 +19,7 @@ def index():
 
 @bp.route("/cv", methods=['GET', 'POST'])
 def cv():
-    return send_file(url_for('static', filename='cv.pdf'))
+    return redirect('https://bamiesking-assets.ams3.digitaloceanspaces.com/cv.pdf')
 
 
 @bp.route("/create_session", methods=['GET', 'POST'])
